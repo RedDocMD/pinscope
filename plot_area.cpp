1320909ad61345500325a3095ee538ee8b0ed93d
Deep Majumder <deep.majumder2019@gmail.com>
2024-01-23 12:06:27 +0530

    Fixed colour scheme

--- plot_area.cpp
+++ plot_area.cpp
@@ -1,5 +1,5 @@
 #include "plot_area.hpp"
-#include <iostream>
+#include <charconv>
 
 namespace pinscope {
 
@@ -12,9 +12,19 @@ PlotArea::PlotArea(const pinscope::Window *data, const bool *enabled,
 
 void PlotArea::on_draw(const Cairo::RefPtr<Cairo::Context> &cr, int width,
                        int height) const {
+  size_t sel_col_idx = 0;
   for (size_t i = 0; i < pin_cnt_; i++) {
     if (enabled_[i]) {
-      cr->set_source_rgb(0, 0, 0);
+      Rgb col;
+      if (sel_col_idx >= sel_colours_.size()) {
+        col = next_colour();
+        sel_colours_.push_back(col);
+      } else {
+        col = sel_colours_[sel_col_idx];
+      }
+      ++sel_col_idx;
+      cr->set_source_rgb(col.red / 255.0f, col.green / 255.0f,
+                         col.blue / 255.0f);
       draw_single_pin(data_[i], cr, width, height);
     }
   }
@@ -54,6 +64,44 @@ std::optional<int> PlotArea::map_y_value(float val, int height) const {
     return {};
   auto frac = (val - min_value) / (max_value - min_value);
   return height - static_cast<int>(height * frac);
+}
+
+static Rgb parse_colour(std::string_view str) {
+  auto ptr = str.data();
+  Rgb colour;
+  std::from_chars(ptr + 1, ptr + 3, colour.red, 16);
+  std::from_chars(ptr + 3, ptr + 5, colour.green, 16);
+  std::from_chars(ptr + 5, ptr + 7, colour.blue, 16);
+  return colour;
+}
+
+Rgb PlotArea::next_colour() const {
+  float max_diff = 0;
+  std::optional<size_t> sel_col_idx;
+  std::optional<Rgb> sel_col;
+  for (size_t i = 0; i < colour_cnt; i++) {
+    if (colour_use_[i] >= min_col_use_)
+      continue;
+    auto colour = parse_colour(colours[i]);
+    float curr_min_diff = std::numeric_limits<float>().max();
+    for (const auto &ot : sel_colours_) {
+      auto diff = colour.distance(ot);
+      curr_min_diff = std::min(curr_min_diff, diff);
+    }
+    if (curr_min_diff > max_diff) {
+      max_diff = curr_min_diff;
+      sel_col_idx = i;
+      sel_col = colour;
+    }
+  }
+  if (sel_col) {
+    ++colour_use_[*sel_col_idx];
+    return *sel_col;
+  } else {
+    ++min_col_use_;
+    ++colour_use_[0];
+    return parse_colour(colours[0]);
+  }
 }
 
 } // namespace pinscope
--- plot_area.hpp
+++ plot_area.hpp
@@ -1,10 +1,31 @@
 #pragma once
 
 #include "window.hpp"
+#include <array>
+#include <cmath>
 #include <gtkmm/drawingarea.h>
 #include <optional>
+#include <string_view>
+#include <vector>
 
 namespace pinscope {
+
+struct Rgb {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+
+  Rgb() = default;
+  Rgb(uint8_t red, uint8_t green, uint8_t blue)
+      : red(red), green(green), blue(blue) {}
+
+  float distance(const Rgb &ot) const {
+    auto rdiff = abs(red - ot.red);
+    auto gdiff = abs(green - ot.green);
+    auto bdiff = abs(blue - ot.blue);
+    return sqrtf(rdiff * rdiff + gdiff * gdiff + bdiff * bdiff);
+  }
+};
 
 class PlotArea : public Gtk::DrawingArea {
 public:
@@ -13,6 +34,12 @@ public:
 private:
   constexpr static float min_value = 0.0f;
   constexpr static float max_value = 5.0f;
+  constexpr static size_t colour_cnt = 14;
+  constexpr static std::array<std::string_view, colour_cnt> colours = {
+      "#ec4899", "#f43f5e", "#f87171", "#fb923c", "#fbbf24",
+      "#a3e635", "#34d399", "#2dd4bf", "#22d3ee", "#38bdf8",
+      "#60a5fa", "#818cf8", "#a78bfa", "#e879f9",
+  };
 
   void on_draw(const Cairo::RefPtr<Cairo::Context> &cr, int width,
                int height) const;
@@ -21,9 +48,15 @@ private:
                        int height) const;
   std::optional<int> map_y_value(float val, int height) const;
 
+  Rgb next_colour() const;
+
   const pinscope::Window *data_;
   const bool *enabled_;
   size_t pin_cnt_;
+
+  mutable std::array<size_t, colour_cnt> colour_use_{0};
+  mutable std::vector<Rgb> sel_colours_{};
+  mutable size_t min_col_use_{0};
 };
 
 } // namespace pinscope
